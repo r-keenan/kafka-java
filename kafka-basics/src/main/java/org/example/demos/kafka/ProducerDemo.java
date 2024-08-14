d5c5548e11dd6438b04d4255a7f06636afe973b3
r-keenan <ross.keenan@protonmail.com>
2024-08-14 09:23:21 -0500

    Updated to move to the kafka folder and not the original folder.

--- kafka-basics/src/main/java/org/example/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/org/example/demos/kafka/ProducerDemo.java
@@ -2,6 +2,6 @@ package org.example.demos.kafka;
 
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello World");
     }
 }
