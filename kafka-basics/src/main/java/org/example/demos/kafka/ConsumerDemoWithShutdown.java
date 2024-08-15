0dd5a21452a9c93ec3ea6013e439b6731b4c052a
r-keenan <ross.keenan@protonmail.com>
2024-08-15 08:01:18 -0500

    Removed unneeded logging line.

--- kafka-basics/src/main/java/org/example/demos/kafka/ConsumerDemoWithShutdown.java
+++ kafka-basics/src/main/java/org/example/demos/kafka/ConsumerDemoWithShutdown.java
@@ -64,7 +64,6 @@ public class ConsumerDemoWithShutdown {
 
             // poll for data
             while (true) {
-                log.info("Polling");
 
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
 
